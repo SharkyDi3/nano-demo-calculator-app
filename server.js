0e9001f89a5949d5dc4e581107b2debafc174414
SharkyDi3 <69460403+SharkyDi3@users.noreply.github.com>
2023-09-12 10:11:05 +0530

    Remove actual answers

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ baseRouter.get('/greeting', (req, res) => {
 
 baseRouter.post('/add', (req, res) => {
     const { num1, num2 } = req.body;
-    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
-        return res.status(400).json({ error: 'Both num1 and num2 should be numbers.'});
-    }
-
     const result = num1 + num2;
     res.json({ result });
 });
@@ -26,10 +22,6 @@ baseRouter.post('/add', (req, res) => {
 
 baseRouter.post('/subtract', (req, res) => {
     const { num1, num2 } = req.body;
-    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
-        return res.status(400).json({ error: 'Both num1 and num2 should be numbers.'});
-    }
-
     const result = num1 - num2;
     res.json({ result });
 });
